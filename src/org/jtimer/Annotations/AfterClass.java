a863726cd2d99bd148f7214eb2a2a102ce0e48f2
MagneticZero <19812604+MagneticZer0@users.noreply.github.com>
2019-09-16 14:54:57 -0400

    License Updates
    Still have some updating to do since the license update. Added the license blurb to all the classes.

--- src/org/jtimer/Annotations/AfterClass.java
+++ src/org/jtimer/Annotations/AfterClass.java
@@ -1,3 +1,22 @@
+/*
+ * JTimer is a Java library that contains various methods and annotations that allow's one to 
+ * time various methods and output it into a graph.
+ * Copyright (C) 2019  Harley Merkaj
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://github.com/MagneticZer0/JTimer/blob/master/LICENSE> 
+ * or <https://www.gnu.org/licenses/>.
+ */
 package org.jtimer.Annotations;
 
 import java.lang.annotation.ElementType;
