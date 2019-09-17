c00ed380bebf825cf220b084daf70e0a5d26743b
MagneticZero <19812604+MagneticZer0@users.noreply.github.com>
2019-09-17 17:47:43 -0400

    Experimental Command Line Things
    I'm seeing if it's possible to run certain code through the command-line. If possible it may be possible to do a one-off thing that times certain code.  This feature may not exist if I deem it too much work to implement.

---
+++ src/org/jtimer/CommandLine.java
@@ -0,0 +1,61 @@
+package org.jtimer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class CommandLine {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		args = new String[]{"-c", "System.out.println(\"Test\");", "-n", "Test", "-r", "6969"};
+		List<String> arguments = Arrays.asList(args);
+		int cIndex = arguments.indexOf("-c");
+		if (cIndex != -1) {
+			if (args.length > cIndex+1) {
+				String name = getArgument(arguments, "-n", "compiledCode");
+				String repetitions =  getArgument(arguments, "-r", "100");
+				String codeToCompile = "import org.jtimer.*;\n"
+									 + "\n"
+									 + "public class CompiledClass {\n"
+									 + "\n"
+									 + "	long counter = 0;\n"
+									 + "\n"
+									 + "	public static void main(String[] args) throws Throwable {\n"
+									 + "		Runner.time(\"CompiledClass.class\");\n"
+									 + "	}\n"
+									 + "\n"
+									 + "	@DisplayName(\"" + name + "\")\n"
+									 + "	@Time(repeat = " + repetitions + ")\n"
+									 + "	public void compiledCode() {\n"
+									 + "		" + args[cIndex+1] + "\n"
+									 + "	}\n"
+									 + "}";
+				System.out.println("Running the supplied code...");
+				runCode(codeToCompile);
+			} else {
+				System.out.println("You must supply code to the -c argument!");
+			}
+		}
+	}
+	
+	public static String getArgument(List<String> args, String argument, String defaultValue) {
+		int argIndex = args.indexOf(argument);
+		if (argIndex != -1) {
+			if (args.size() > argIndex+1) {
+				return args.get(argIndex+1);
+			}
+		}
+		return defaultValue;
+	}
+
+	public static void runCode(String code) throws FileNotFoundException {
+		int random = (int) Math.round(Math.random()*Integer.MAX_VALUE);
+		File uncompiled = new File(System.getProperty("java.io.tmpdir") + "compiledCode" + random + ".java");
+		uncompiled.deleteOnExit();
+		PrintWriter writer = new PrintWriter(uncompiled);
+		writer.print(code);
+		writer.close();
+	}
+}
